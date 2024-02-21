b683a357987bf11fca3cbc2541eada458cc9e8f8
Mohamad Arsya Kaukabi <arsyakaukabi@gmail.com>
2024-02-21 07:46:08 +0700

    add:konva

---
+++ app/posts/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Posts() {
+  return (
+    <h1>Post page</h1>
+  );
+}
---
+++ components/canvas.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Stage, Layer, Star, Text, Circle} from 'react-konva';
+
+// function Canvas(props) {
+//   return (
+//     <Stage width={window.innerWidth} height={window.innerHeight}>
+//       <Layer>
+//         <Circle x={200} y={100} radius={50} fill="blue" />
+
+//       </Layer>
+//     </Stage>
+//   );
+// }
+
+// export default Canvas;
+
+
+
+function generateShapes() {
+  return [...Array(10)].map((_, i) => ({
+    id: i.toString(),
+    x: Math.random() * window.innerWidth,
+    y: Math.random() * window.innerHeight,
+    // rotation: Math.random() * 180,
+    isDragging: false,
+  }));
+}
+
+const INITIAL_STATE = generateShapes();
+
+const Canvas = () => {
+  const [stars, setStars] = React.useState(INITIAL_STATE);
+
+  const handleDragStart = (e) => {
+    const id = e.target.id();
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: star.id === id,
+        };
+      })
+    );
+  };
+  const handleDragEnd = (e) => {
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: false,
+        };
+      })
+    );
+  };
+
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Text text="Try to drag a star" />
+        {stars.map((star) => (
+          <Star
+            key={star.id}
+            id={star.id}
+            x={star.x}
+            y={star.y}
+            numPoints={5}
+            innerRadius={20}
+            outerRadius={40}
+            fill="#89b717"
+            opacity={0.8}
+            draggable
+            rotation={star.rotation}
+            shadowColor="black"
+            shadowBlur={10}
+            shadowOpacity={0.6}
+            shadowOffsetX={star.isDragging ? 10 : 5}
+            shadowOffsetY={star.isDragging ? 10 : 5}
+            scaleX={star.isDragging ? 1.2 : 1}
+            scaleY={star.isDragging ? 1.2 : 1}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+          />
+        ))}
+      </Layer>
+    </Stage>
+  );
+};
+
+export default Canvas
